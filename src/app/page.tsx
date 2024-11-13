1e92fa599d4d09f5723008c20604379e6e8178a9
Habib <pbllemb@gmail.com>
2024-11-13 16:08:53 +0100

    Moving prompt-input and button in a separate component from the Home(), modifying css for displaying it in a right way

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,24 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+function PromptInput(){
+	 return (
+		  <div className={styles.promptInput}>
         <TextField
           label="Enter your prompt"
           variant="outlined"
           multiline
 			/>
         <Button variant="contained" endIcon={<SendIcon />}/>
+		  </div>
+	 )
+}
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
       </main>
+      <PromptInput />
     </div>
   );
 }
