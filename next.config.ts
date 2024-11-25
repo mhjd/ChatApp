2c0742dc9e04b991b7485a1e70d69508cac70e95
Habib <pbllemb@gmail.com>
2024-11-25 20:02:34 +0100

    update packages configuration, because it didn't work on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['@mui/material', '@mui/icons-material'],
 };
 
 export default nextConfig;
